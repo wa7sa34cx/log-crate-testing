1f3f3ff8aaedf617200f654be0db4ba7ee385d17
Alex Black <wa7@sa34.cx>
2021-12-12 00:01:50 +0300

    Testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod logging;
+
+// extern crate pretty_env_logger;
+#[macro_use] extern crate log;
+
 fn main() {
-    println!("Hello, world!");
+    // pretty_env_logger::init();
+    pretty_env_logger::init_timed();
+
+    trace!("tracing...");
+    info!("such information");
+    warn!("o_O");
+    error!("much error");
+
+    nested::run();
+
+    info!(env!("CARGO_PKG_NAME"));
+}
+
+mod nested {
+    pub fn run() {
+        trace!("qwerty");
+    }
 }
